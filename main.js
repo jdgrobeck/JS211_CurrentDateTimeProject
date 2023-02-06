66e8d39415a2d81e3304b90c61c786d9af7630cf
jdgrobeck <jdgrobeck@gmail.com>
2023-02-06 16:09:47 -0600

    initial commit

--- main.js
+++ main.js
@@ -16,35 +16,133 @@ const displayDate = () => {
 
 // Write a JavaScript program to convert a number to a string.
 
+const numToString = () => {
+  
+  let num = document.getElementById("num-to-str").value;
+  let text = num.toString();
+
+  document.getElementById("number").innerHTML = text;
+
+} 
 
 
 // Write a JavaScript program to convert a string to the number.
 
+const stringToNum = () => {
+  
+  let str = document.getElementById("str-to-num").value;
+  let num = parseInt(str);
+
+  document.getElementById("results").innerHTML = num;
+
+} 
+
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-  // * Boolean
+  
+// * Boolean
+const findTypeOf = () => {
+let a = true;
+console.log(`a is a ${typeof a}`);
+
+let b = null
+console.log('b is an object set to null');
+
+let c;
+console.log(`c is a ${typeof c}`);
+
+let d = 32
+console.log(`d is a ${typeof d}`);
+
+let e = isNaN('Hello')
+  if (e == true) {
+    console.log(`e is a NaN`);
+  }
+
+  f = "Hello"
+
+  let h2 = document.getElementById("practice");
+
+  let html = `<p>a is a ${typeof a}</p>
+             <p>b is an object set to null</p>
+             <p>c is a ${typeof c}</p>
+             <p>d is a ${typeof d}</p>
+             <p>e is a NaN</p>
+             <p>f is a ${typeof f}</p>`
+
+  h2.insertAdjacentHTML("afterend", html);
+
+}
+  
+
+
+
+
+
   // * Null
   // * Undefined
   // * Number
   // * NaN
   // * String
+
   
 
   
 // Write a JavaScript program that adds 2 numbers together.
+const addTwoNumbers = () => {
 
+  let numOne = document.getElementById('num1').valueAsNumber
+  let numTwo = document.getElementById('num2').valueAsNumber
 
+  sum = numOne + numTwo
+  
+  document.getElementById("sum-of-numbers").innerHTML = sum;
+
+}
 
 // Write a JavaScript program that runs only when 2 things are true.
+const bothTrue = () => {
+  let numThree = document.getElementById('num3').valueAsNumber;
+  let numFour = document.getElementById('num4').valueAsNumber;
 
+  if (numThree > 50 && numFour > 50) {
+    document.getElementById("greater-than-fifty").innerHTML = "Yes"
+  }
 
+  else {
+    document.getElementById("greater-than-fifty").innerHTML = "No"
+  }
+
+}
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
+const oneTrue = () => {
 
+  if (document.getElementById("umbrella-yes").checked) {
+    document.getElementById("umbrella-results").innerHTML = "You need an umbrella."
+  }
 
+  else {
+    document.getElementById("umbrella-results").innerHTML = "You don't need an umbrella."
+  }
+  
+}
 
-// Write a JavaScript program that runs when both things are not true.  
+// Write a JavaScript program that runs when both things are not true.
+const bothFalse = () => {
+ 
+  if (document.getElementById("power-no").checked && document.getElementById("wifi-no").checked){
+    document.getElementById("power-results").innerHTML = "You can't attend class."
+  }
+
+  else (
+    document.getElementById("power-results").innerHTML = "You probably can make it work, but nothing is certain."
+  )
+  
+
+}
+
 
 // ***************************
 //         PART TWO
